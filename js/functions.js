e17136a2c6dadde151cb63ca2f4605a0f0383d25
Morvana Bonin <morvanabonin@gmail.com>
2013-04-09 00:35:21 -0300

    Acrescimo do logo, media queries quase prontas.

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,9 @@
-!function (window, undefined){
-	"use strict";
-
-	window.onload = function() {
-		document.menuscreen = MenuScreen();
-	}
-
-	//document.location.reload();
-
-	function MenuScreen() {
-		if (screen.width <=320 || document.width <=320) {
-			document.getElementById("navbar").className = "navbar navbar-fixed-top";
-			document.getElementById("navbar-inner").className = "navbar-inner";
-			document.getElementById("btn-navbar").className ="btn btn-navbar";
-		}
-
-	}
+!function ($){
 	
-}(this);
+    "use strict";
+
+    $('.anchorLink').click(function() {
+        $('.nav-collapse').collapse('toggle');
+    });
+
+}(window.jQuery);
