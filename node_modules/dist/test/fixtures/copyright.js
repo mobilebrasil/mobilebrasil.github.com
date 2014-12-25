63e0b6550b80827bda028443cbfbf5c8b81d69e0
Morvana Bonin <morvanabonin@gmail.com>
2014-12-25 12:38:23 -0200

    Delete da pasta node_modules do repositório, acréscimo e configuração do arquivo package.json e modificação no bower.json.

--- node_modules/dist/test/fixtures/copyright.js
+++
@@ -1,6 +0,0 @@
-/*!
-  * Person.js - person class 
-  * v0.1.0
-  * copyright JGA 2012
-  * MIT License
-  */
--- node_modules/dist/test/fixtures/input.js
+++
@@ -1,8 +0,0 @@
-var Person = function(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-}
-
-Person.prototype.getFullName = function() {
-  return this.firstName + ' ' + this.lastName;
-}
