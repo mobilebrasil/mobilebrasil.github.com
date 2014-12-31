8fd14fb8d65a364ca5305bd53ae53d2de306b129
Morvana Bonin <morvanabonin@gmail.com>
2014-12-30 23:37:42 -0200

    Criação do menu e menu responsivo, está com border com cores para referência e teste.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,13 @@
 +function ($) {
   'use strict';
+
+  $(function () {
+  	$('[data-toggle="tooltip"]').tooltip()
+  })
   
+  $('.menu-door').on('click', function(){
+  	$('#sidebar-wrapper').toggle();
+  	$('.glyphicon').toggleClass('glyphicon-chevron-left glyphicon-chevron-right');
+  });
 
 }(window.jQuery);
